9869d30bc51ac3cf8026d21fce8323c3e7d7d5b3
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-01 19:38:05 +1300

    Add controllers manipulating the database.

---
+++ refactor-me/Controllers/ProductOptionsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using refactor_me.Models;
+
+namespace refactor_me.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductOptionsController : ApiController
+    {
+        private ProductOptionRepository db = new ProductOptionRepository();
+
+        // 1. GET: products/productId/options
+        [Route("{productId}/options")]
+        [HttpGet]
+        public IQueryable<ProductOption> GetProductOptions(Guid productId)
+        {
+            return db.GetAll().Where(m => m.ProductId == productId);
+        }
+
+        // 2. GET: products/productId/options/id
+        [Route("{productId}/options/{id}")]
+        [HttpGet]
+        [ResponseType(typeof(ProductOption))]
+        public IHttpActionResult GetProductOption(Guid productId, Guid id)
+        {
+            ProductOption productOption = db.GetOne(id);
+
+            if (productOption == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productOption);
+        }
+
+        // 3. POST: products/productId/options
+        [Route("{productId}/options")]
+        [HttpPost]
+        [ResponseType(typeof(ProductOption))]
+        public IHttpActionResult PostProductOption(Guid productId, ProductOption productOption)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            try
+            {
+                productOption.ProductId = productId;
+                db.Add(productOption);
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductOptionExists(productOption.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return GetProductOption( productId, productOption.Id);
+        }
+
+
+
+        // 4. PUT: products/productId/options/id
+        [Route("{productId}/options/{id}")]
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductOption(Guid id, ProductOption productOption)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productOption.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                db.Update(id,productOption);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductOptionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // 5. DELETE: products/productId/options/id
+        [Route("{productId}/options/{id}")]
+        [HttpDelete]
+        [ResponseType(typeof(ProductOption))]
+        public IHttpActionResult DeleteProductOption(Guid id)
+        {
+            ProductOption productOption = db.GetOne(id);
+
+            if (productOption == null)
+            {
+                return NotFound();
+            }
+
+            db.Delete(productOption);
+
+            return Ok(productOption);
+        }
+
+        private bool ProductOptionExists(Guid id)
+        {
+            return db.Table.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ refactor-me/Controllers/ProductsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using refactor_me.Models;
+
+namespace refactor_me.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductsController : ApiController
+    {
+        private ProductRepository db = new ProductRepository();
+
+        // 1. GET: Products
+        [Route]
+        [HttpGet]
+        public IQueryable<Product> GetProducts()
+        {
+            return db.GetAll();
+        }
+
+        // 2. GET: Products/name
+        [Route]
+        [HttpGet]
+        public IQueryable<Product> GetByName(string name)
+        {
+            return db.GetByName(name);
+        }
+
+        // 3. GET: Products/5
+        [Route("{id}")]
+        [HttpGet]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(Guid id)
+        {           
+            Product product = db.GetOne(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // 4. POST: Products
+        [Route]
+        [HttpPost]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                db.Add(product);
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductExists(product.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return GetProduct(product.Id);
+        }
+
+        // 5. PUT: Products/5
+        [Route("{id}")]
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(Guid id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                db.Update(id, product);               
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // 6. DELETE: api/Products/5
+        [Route("{id}")]
+        [HttpDelete]
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult DeleteProduct(Guid id)
+        {
+            Product product = db.GetOne(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Delete(product);
+
+            return Ok(product);
+        }
+
+        private bool ProductExists(Guid id)
+        {
+            return db.Table.Count(e => e.Id == id) > 0;
+        }
+    }
+}
